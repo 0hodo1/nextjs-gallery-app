4d0f17dd69294abee783b27020e3ac02a172ba04
Ömer Hodo <omerhodo@gmail.com>
2022-03-25 11:29:38 +0300

    firebase implemantation finished

---
+++ components/Title.js
@@ -0,0 +1,10 @@
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Hodo Gallery App</h1>
+      <h2>All Photos</h2>
+      <p>Welcome to your gallery</p>
+    </div>
+  );
+};
+export default Title;
---
+++ firebase/index.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsPUnd0Gy3tmstNdZ-Q6Hro5SeNCx2FeA",
+  authDomain: "modern-react-app-b5454.firebaseapp.com",
+  projectId: "modern-react-app-b5454",
+  storageBucket: "modern-react-app-b5454.appspot.com",
+  messagingSenderId: "58268936893",
+  appId: "1:58268936893:web:3a1afa5dfede664edee5d4",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const storage = getStorage();
+
+export { db, storage };
